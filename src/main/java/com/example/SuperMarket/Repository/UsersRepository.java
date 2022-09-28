6cdbcff4d841a2f1036b327d5e2d8dc7192362f5
joseph221 <mrdavidjoseph60@gmail.com>
2022-09-28 12:53:16 +0300

    initial Admin pass

--- src/main/java/com/example/SuperMarket/Repository/UsersRepository.java
+++ src/main/java/com/example/SuperMarket/Repository/UsersRepository.java
@@ -1,6 +1,9 @@
 package com.example.SuperMarket.Repository;
 
 import com.example.SuperMarket.model.Users;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +13,9 @@ public interface UsersRepository extends JpaRepository<Users,Long>{
     
     @Query(value = "select * from users where username=?1 and password=?2",nativeQuery = true)
     public Users getLogins(String username,String password);
+
+    @Query(value = "select * from users",nativeQuery = true)
+    public List<Users> isUser();
 
     @Transactional
     @Modifying
--- src/main/java/com/example/SuperMarket/model/Constants.java
+++ src/main/java/com/example/SuperMarket/model/Constants.java
@@ -1,19 +1,45 @@
 package com.example.SuperMarket.model;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.SuperMarket.Dto.UserDto;
 import com.example.SuperMarket.Repository.CompanyConfRepository;
+import com.example.SuperMarket.Repository.RoleRepository;
+import com.example.SuperMarket.Repository.UsersRepository;
+import com.example.SuperMarket.service.UserService;
 
 @Configuration
 public class Constants {
    
     @Bean
-   CommandLineRunner commandLineRunner(CompanyConfRepository companyConfRepository){
+   CommandLineRunner commandLineRunner(CompanyConfRepository companyConfRepository,
+   UsersRepository usersRepository,RoleRepository roleRepository,UserService userService){
        return args ->{
+            List<Roles> roles1 = roleRepository.getByRoleId(1);
+            if(roles1.isEmpty()){
+                Roles role = new Roles();
+                role.setRoleId(1);
+                role.setRoleName("Admin");
+                roleRepository.save(role);
+            }
+
+            List<Users> isUserAvailable = usersRepository.isUser();
+            if(isUserAvailable.isEmpty()){
+                UserDto user = new UserDto();
+                user.setFirstname("Admin");
+                user.setLastname("Admin");
+                user.setUsername("mrdavidjoseph60@gmail.com");
+                user.setEmail("mrdavidjoseph60@gmail.com");
+                user.setPassword("super$Admin1");
+                user.setRoleId(1);
+                userService.save(user);
+            }
+
             Optional<CompanyConfiguration> isPresent = companyConfRepository.getCompanyConfig();
             if(isPresent.isEmpty()){
                 CompanyConfiguration intials = new CompanyConfiguration(
